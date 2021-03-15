1e46655a23cd1082c86a99ea0527210c5e385aa9
Rbalbino <rafael.neco@hotmail.com>
2021-03-15 20:31:34 -0300

    segunda tentativa

---
+++ src/test/java/simples/Artigo.java
@@ -0,0 +1,43 @@
+package simples;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertTrue;
+
+public class Artigo {
+    String url;             //Endereço site alvo
+    WebDriver driver;       //Objeto do Selenium Webdriver
+
+    @Before                 //Antes do teste
+    public void iniciar(){
+        url = "https://pt.wikipedia.org/";
+        System.setProperty("webdriver.chrome.driver", "drivers/chrome/89/chromedriver.exe"); // Onde está o chromedriver
+        driver = new ChromeDriver();  // Instanciasr o Selenium como Chrome Driver
+        driver.manage().window().maximize(); // Maximizare a janela do navegador
+        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS); // Define uma espera implicida de 1 min, verificando o carregamento a cada milissegundo
+    }
+
+    @Test                           //Durante o teste
+    public void consultarArtigo(){
+        // Abrir o site
+        driver.get(url);
+
+        // Pesquisar por "Ovo de Páscoa"
+        driver.findElement(By.id("searchInput")).sendKeys("Ovo de Páscoa");
+        driver.findElement(By.id("searchButton")).click(); // Clicar na lupa
+
+        // Validar o titulo da página de retorno
+        //        assertEquals("Ovo de Páscoa - Wikipédia, a enciclopédia livre", driver.getTitle());
+        assertTrue(driver.getTitle().contains("Ovo de Páscoa"));
+    }
+
+    @After                      //Depois do teste
+    public void finalizar(){
+        driver.quit();
+    }
+}
